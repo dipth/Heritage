146f6a56635137975547bfe505fca688ec34b771
Thomas Dippel <thomas@dippel.dk>
2011-11-01 16:40:06 +0100

    Removed trailing whitespace

--- lib/heritage/active_record/acts_as_heir.rb
+++ lib/heritage/active_record/acts_as_heir.rb
@@ -1,19 +1,19 @@
 module Heritage
   module ActiveRecord
     module ActsAsHeir
-      
+
       def acts_as_heir_of(predecessor_symbol)
         extend ClassMethods
         include InstanceMethods
-        
+
         class_attribute :_predecessor_klass, :_predecessor_symbol
         self._predecessor_symbol = predecessor_symbol
         self._predecessor_klass = Object.const_get(predecessor_symbol.to_s.capitalize)
-        
+
         has_one :predecessor, :as => :heir, :class_name => predecessor_symbol.to_s.capitalize, :autosave => true, :dependent => :destroy
 
         alias_method_chain :predecessor, :build
-        
+
         # Expose columns from the predecessor
         self._predecessor_klass.columns.reject{|c| c.primary || c.name =~ /^heir_/}.map(&:name).each do |att|
           define_method(att) do
@@ -23,7 +23,7 @@ module Heritage
             predecessor.send("#{att}=",val)
           end
         end
-        
+
         # Expose associations from the predecessor
         self._predecessor_klass.reflect_on_all_associations.reject{|a| a.name == :heir}.each do |association|
           define_method(association.name) do
@@ -33,7 +33,7 @@ module Heritage
             predecessor.send("#{association.name}=",val)
           end
         end
-        
+
         # We need to make sure that updated_at values in the predecessor table is updated when the heir is saved.
         after_update :touch_predecessor
         
@@ -48,21 +48,21 @@ module Heritage
           end
         end
       end
-      
+
       module ClassMethods
-        
+
       end
-      
+
       module InstanceMethods
         def predecessor_with_build(attributes = {})
           predecessor_without_build || build_predecessor(attributes)
         end
-        
+
         def touch_predecessor
           predecessor.touch
         end
       end
-      
+
     end
   end
 end
--- lib/heritage/active_record/acts_as_predecessor.rb
+++ lib/heritage/active_record/acts_as_predecessor.rb
@@ -1,31 +1,31 @@
 module Heritage
   module ActiveRecord
     module ActsAsPredecessor
-      
+
       def acts_as_predecessor(options = {})
         extend ClassMethods
         include InstanceMethods
-        
+
         options[:exposes] ||= []
         class_attribute :_acts_as_predecessor_settings
         self._acts_as_predecessor_settings = options
         
         belongs_to :heir, :polymorphic => true, :touch => true
       end
-      
+
       module ClassMethods
-        
+
         def get_heritage_exposed_methods
           result = self._acts_as_predecessor_settings[:exposes]
           result.is_a?(Array) ? result : [result]
         end
-        
+
       end
-      
+
       module InstanceMethods
         
       end
-      
+
     end
   end
 end
