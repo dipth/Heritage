9225358de3625f1d60a24b1fe13c131ffb043dad
Jered Odegard <jered@soromi.com>
2011-12-02 10:24:32 -0800

    Updated blog_post spec to read better

--- heritage_demo/spec/models/blog_post_spec.rb
+++ heritage_demo/spec/models/blog_post_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe BlogPost do
   let(:blog_post) { Factory(:blog_post) }
   
-  it "should inherit title attribute" do
+  it "should inherit attributes" do
     blog_post.should respond_to :title
   end
   
